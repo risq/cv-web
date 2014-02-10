8b0302c0dcd65218edcaeefab53cf31f39105981
risq <valentin.ledrapier@gmail.com>
2014-02-11 00:16:02 +0100

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,26 +1,54 @@
 var scrollTo = null;
+var lastScrollPos = 0;
+
+
 
 var $pf = $('#pf-items').isotope({
 	// options
 	itemSelector: '.item-container',
-	layoutMode: 'masonry',
+	layoutMode: 'masonry'
 });
 
 
 $('#pf-items .item').click(function(){
-	$('.item-container.opened').removeClass('opened');
-	$(this).parent().addClass('opened');
-	$('#pf-items').isotope();
-	scrollTo = $(this);
+	if ($(this).parent().hasClass('opened')) {
+			$('.item-container.opened').removeClass('opened').each(function(){
+				$(this).find('img').resizeToParent();
+			});
+			$('#pf-items').isotope();
+	}
+	else {
+		lastScrollPos = $(document).scrollTop();
+		$('.item-container.opened').removeClass('opened').each(function(){
+			$(this).find('img').resizeToParent();
+		});
+		$(this).fadeOut(200, function(){
+			$(this).fadeIn().parent().addClass('opened');
+			$(this).find('img').resizeToParent();
+			$('#pf-items').isotope();
+		})
+		scrollTo = $(this);
+	}
 })
 
 $pf.isotope( 'on', 'layoutComplete', scrollToOpened );
 
+imagesLoaded( '.img-container', function() {
+	$('.img-container img').resizeToParent();
+});
+
 function scrollToOpened( isoInstance, laidOutItems ) {
 	if (scrollTo !== null) {
 		$('html, body').animate({
-	        scrollTop: scrollTo.offset().top - 120
-	    }, 300);
+	        scrollTop: scrollTo.offset().top - 78
+	    }, 500);
+	    scrollTo.fadeIn();
 	    scrollTo = null;
-	}	
+	}
+	else if (lastScrollPos !== 0) {
+		$('html, body').animate({
+	        scrollTop: lastScrollPos
+	    }, 500);
+	    lastScrollPos = 0;
+	}
 }
